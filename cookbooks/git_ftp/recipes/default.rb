e51c8d2dcd3bd2ea44a4a34a87dcb08f44f6cec6
Darcy Brown <darcy@dbcodeproject.com>
2010-06-26 10:49:50 -0500

    updated to pull from latest git-ftp

--- cookbooks/git_ftp/recipes/default.rb
+++ cookbooks/git_ftp/recipes/default.rb
@@ -2,8 +2,8 @@
 # Recipe:: default
 if ['solo', 'app', 'app_master', 'util'].include?(node[:instance_role])
 
-  package="darcy-git-ftp-0.0.8-0-g6abdb4b"
-  package_folder="darcy-git-ftp-dc9b4b1"
+  package="darcy-git-ftp-0.0.9-0-g23921a3"
+  package_folder="darcy-git-ftp-23921a3"
   dir = "/usr/local/git-ftp"
   
   directory dir do
@@ -17,20 +17,19 @@ if ['solo', 'app', 'app_master', 'util'].include?(node[:instance_role])
   execute "install-git-ftp" do
     command %Q{
       
-      curl -L http://github.com/darcy/git-ftp/tarball/0.0.8 -o #{package}.tgz &&
+      curl -L http://github.com/darcy/git-ftp/tarball/0.0.9 -o #{package}.tgz &&
       tar zxvf #{package}.tgz &&
       mv #{package_folder}/* #{dir}/. &&
       rm #{package}.tgz &&
       rm #{package_folder}
     }
-    not_if { File.exists?("#{dir}/git-ftp.sh") }
+    not_if { File.exists?("#{dir}/git-ftp") }
   end
   
   execute "add-to-path" do
     command %Q{
       mkdir -p #{dir}/bin &&
-      chmod 755 #{dir}/bin/git-ftp.sh &&
-      ln -nfs #{dir}/git-ftp.sh #{dir}/bin/git-ftp &&
+      ln -nfs #{dir}/git-ftp #{dir}/bin/git-ftp &&
       echo 'export PATH="$PATH:#{dir}/bin"' >> /etc/profile
     }
     not_if "grep 'export PATH=\"$PATH:#{dir}/bin\"' /etc/profile"
